f4cfcb9db5025473e0fe6249a49f80dbbb5198f5
Katrina Pritchard <pritckat@gmail.com>
2018-09-05 20:57:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,3 +8,11 @@ class application
       item_name = req.path.split("/items/").last
 
       item = Item.all.find {|i| i.name == item_name}
+      resp.write item.price
+    else
+      resp.write "Item not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
