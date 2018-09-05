13a8631764ea43e6724f0c4c432c7a5278b55722
Katrina Pritchard <pritckat@gmail.com>
2018-09-05 21:08:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
       # else
       #   resp.write "Item not found"
       #   resp.status = 400
-      # end
+       end
     else
       resp.write "Route not found"
       resp.status = 404
