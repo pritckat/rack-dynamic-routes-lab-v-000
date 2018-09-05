132aaef12d85045e4756216512f3782813b3972c
Katrina Pritchard <pritckat@gmail.com>
2018-09-05 20:55:48 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+class application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      item = Item.all.find {|i| i.name == item_name}
